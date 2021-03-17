6f8019f39f518b916bf2201ea39a9f1af609d59e
Guido Martinez <martinez.guido14@gmail.com>
2021-03-16 21:36:22 -0300

    fin seccion 3 diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graph1Component } from './pages/graph1/graph1.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'graph1', component: Graph1Component },
+      { path: 'progress', component: ProgressComponent },
+      { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+    ],
+  },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  { path: '**', component: NopagefoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/graph1/graph1.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graph',
+  templateUrl: './graph1.component.html',
+  styles: [],
+})
+export class Graph1Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
